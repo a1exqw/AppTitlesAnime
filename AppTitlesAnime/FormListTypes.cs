8e9b7f65515d14e14e6429be9b5281c027d4936f
Нагаев Алекcандр <nagaev1117@mail.ru>
2025-04-08 10:52:26 +0500

    Добавлены формы для работы со справочником Типы аниме

---
+++ AppTitlesAnime/FormListTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTitlesAnime
+{
+    public partial class FormListTypes : Form
+    {
+        public FormListTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddType_Click(object sender, EventArgs e)
+        {
+            FormAddType formAddType = new FormAddType();
+            formAddType.ShowDialog();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
